8226330f868a91b3d7b0e615d597b9682efa715a
judinus10 <judes.20222403@iit.ac.lk>
2024-12-02 12:00:48 +0530

    number clarification

---
+++ src/numberClarification.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class numberClarification {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String x = "y"; // Initial value to enter the loop
+
+        while (x.equalsIgnoreCase("Y")) { // Compare using equalsIgnoreCase
+            System.out.print("Enter Number: ");
+            int num = input.nextInt();
+
+            // Check if the number is zero, negative, or positive
+            if (num == 0) {
+                System.out.println("Zero");
+            } else if (num < 0) {
+                System.out.println("Negative");
+            } else {
+                System.out.println("Positive");
+            }
+
+            // Ask if the user wants to continue
+            System.out.print("Do you want to continue? (Y/N): ");
+            x = input.next(); // Get user input
+
+            // Exit condition for "N"
+            if (x.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+        System.out.println("Program ended. Goodbye!");
+    }
+}
